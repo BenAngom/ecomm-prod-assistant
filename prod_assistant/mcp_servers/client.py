b29c8ad7353385bf0a23c198af9de7a302f587ca
benangom <benudhor@gmail.com>
2026-03-11 00:49:30 +0530

    mcp integration

--- prod_assistant/mcp_servers/client.py
+++ prod_assistant/mcp_servers/client.py
@@ -5,9 +5,10 @@ async def main():
     client = MultiServerMCPClient({
         "hybrid_search": {   # server name
             "command": "python",
-            "args": [
-                r"D:\complete_content_new\llmops-batch\ecomm-prod-assistant\prod_assistant\mcp_servers\product_search_server.py"
-            ],  # absolute path
+            # "args": [
+            #     r"D:\complete_content_new\llmops-batch\ecomm-prod-assistant\prod_assistant\mcp_servers\product_search_server.py"
+            # ],  # absolute path
+            "args": ["-m", "prod_assistant.mcp_servers.product_search_server"],
             "transport": "stdio",
         }
     })
